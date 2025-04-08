9e9a6d082a3c8464956ead5c8cdaf7cff3b4e8b7
Popes117 <pedroafonsoml@gmail.com>
2025-04-08 17:45:00 +0100

    Deu?

---
+++ build/objects/VI-RT-V4-PathTracing/Scene/scene.d
@@ -0,0 +1,16 @@
+build/objects/VI-RT-V4-PathTracing/Scene/scene.o: \
+ VI-RT-V4-PathTracing/Scene/scene.cpp \
+ VI-RT-V4-PathTracing/Scene/scene.hpp \
+ VI-RT-V4-PathTracing/Primitive/primitive.hpp \
+ VI-RT-V4-PathTracing/Primitive/Geometry/geometry.hpp \
+ VI-RT-V4-PathTracing/Primitive/BB.hpp \
+ VI-RT-V4-PathTracing/utils/vector.hpp VI-RT-V4-PathTracing/Rays/ray.hpp \
+ VI-RT-V4-PathTracing/utils/RGB.hpp \
+ VI-RT-V4-PathTracing/Rays/intersection.hpp \
+ VI-RT-V4-PathTracing/Primitive/BRDF/BRDF.hpp \
+ VI-RT-V4-PathTracing/Primitive/BRDF/BRDF.hpp \
+ VI-RT-V4-PathTracing/Light/light.hpp \
+ VI-RT-V4-PathTracing/Light/AreaLight.hpp \
+ VI-RT-V4-PathTracing/Light/light.hpp \
+ VI-RT-V4-PathTracing/Primitive/Geometry/triangle.hpp \
+ VI-RT-V4-PathTracing/Primitive/Geometry/geometry.hpp
