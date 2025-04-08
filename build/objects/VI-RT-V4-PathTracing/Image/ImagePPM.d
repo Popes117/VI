663e353763e287fd20dc056d79978de6a015e057
Popes117 <pedroafonsoml@gmail.com>
2025-04-08 18:04:41 +0100

    Setup top

--- build/objects/VI-RT-V4-PathTracing/Image/ImagePPM.d
+++
@@ -1,4 +0,0 @@
-build/objects/VI-RT-V4-PathTracing/Image/ImagePPM.o: \
- VI-RT-V4-PathTracing/Image/ImagePPM.cpp \
- VI-RT-V4-PathTracing/Image/ImagePPM.hpp \
- VI-RT-V4-PathTracing/Image/image.hpp VI-RT-V4-PathTracing/utils/RGB.hpp
